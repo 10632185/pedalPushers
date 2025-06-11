a70f63c0278f1576f00ad5d02f0a56d98efc9fb1
10632185 <10632185@ucn.dk>
2025-06-11 14:38:29 +0200

    Basic JS

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+const domain = "https://pedalpushers.xn--sebastianlynghj-jub.dk/"
+const postEndPoint = "wp-json/wp/v2/posts";
+
+
+/* Definere vores cykelnavne og cykelpriser */
+const cykelNavne = ["Kildemoes Herrecykel", "Specialized Allez Racer", "Mbk Concept Herrecykel", "Hardy Classic", "Kildemoes Classic", "Rock Machine Blackout Elcykel", "Sco", "Nishiki", "Von Backhaus", "Classic Centurion", "Cannondale Racer", "Cube RFR Mth Hardtail", "Centurion Classic Steel", "Cannondale Quick Hybrid", "Colnago Cafe Racer", "Gt Touring Mtb", "Sco Comfort", "Nishiki Touring Master", "Principia Gaia Racercykel", "Centurion Helium Racer", "Cube Attain Pro" ];
+console.log(cykelNavne)
+const cykelPriser = [2800, 4000, 1500, 1500, 1500, 7200, 800, 2800, 1900, 1700, 3500, 2600, 3000, 3800, 4000, 2600, 1500, 2000, 2900, 3600, 5500]
+console.log(cykelPriser)
+
+const cykelImg = []
